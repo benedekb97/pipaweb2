0e185ee00999d6ccca0387f2a0656fdc9d5489a1
benedekb97 <benedekb97@gmail.com>
2017-05-11 21:59:22 +0200

    Sanyi meleg

--- register.php
+++ register.php
@@ -1,7 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: beni
- * Date: 2017.05.11.
- * Time: 21:48
- */
+session_start();
+
+include("includes/init.php");
+
+include("includes/Users.php");
+include("includes/Locations.php");
+include("includes/Pipes.php");
+include("includes/Log.php");
+
+
+$users = new Users();
+$locations = new Locations();
+$pipes = new Pipes();
+$log = new Log(0, "index", "view");
+
+
+include("includes/current_user.php");
+
+if(isset($current_user)){
+    die("403 - Not authorized!");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Felhasználónév igénylése</title>
+    <?php include("includes/head.php"); ?>
+</head>
+<body>
+<div class="container">
+    <?php include("includes/nav.php"); ?>
+    <div class="row">
+        <div class="col-lg-6 col-lg-push-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+
+                </div>
+                <div class="panel-body">
+
+                </div>
+                <div class="panel-footer">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
