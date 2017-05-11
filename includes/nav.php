a4e58a5dc4c8155bd5706754f742489c3d0d254c
benedekb97 <benedekb97@gmail.com>
2017-05-11 13:10:12 +0200

    created default style for all webpages

---
+++ includes/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Pipa.ml</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="pipes">Anyád</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="login">Bejelentkezés</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -11,4 +11,46 @@ include("includes/Log.php");
 $users = new Users();
 $locations = new Locations();
 $pipes = new Pipes();
-$log = new Log(0,"index", "view");
+$log = new Log(0, "index", "view");
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <?php include("includes/head.php"); ?>
+</head>
+<body>
+<div class="container">
+    <?php include("includes/nav.php"); ?>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Anyád</h3>
+                </div>
+                <div class="panel-body">
+                    Buzi vagy
+                </div>
+                <div class="panel-footer">
+                    <button type="button" class="btn btn-default">Anyád</button>
+                    <button type="button" class="btn btn-primary">Anyád</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Anyád</h3>
+                </div>
+                <div class="panel-body">
+                    Buzi vagy
+                </div>
+                <div class="panel-footer">
+                    <button type="button" class="btn btn-default">Anyád</button>
+                    <button type="button" class="btn btn-primary">Anyád</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
