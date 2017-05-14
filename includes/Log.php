8a1d0a147beb804b0d279c8c620915567b3b6cdc
benedekb97 <benedekb97@gmail.com>
2017-05-15 00:17:07 +0200

    removed test code

--- includes/Log.php
+++ includes/Log.php
@@ -14,14 +14,14 @@ class Log
         $timestamp = time()-10;
         $ip = $_SERVER['REMOTE_ADDR'];
         $query = $mysql->query("SELECT * FROM logs WHERE user_id='$user_id' AND ip='$ip' AND timestamp>'$timestamp'");
-        /*if($query->num_rows == 0){
+        if($query->num_rows == 0){
             $this->user_id = $user_id;
             $this->ip = $ip;
             $this->page = $page;
             $this->data_changed = $data_changed;
             $this->timestamp = time();
             $mysql->query("INSERT INTO logs (user_id, ip, page, data_changed, timestamp) VALUES ('$this->user_id','$this->ip','$this->page','$this->data_changed','$this->timestamp')");
-        }*/
+        }
     }
 
 
