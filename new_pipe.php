bf4d93566c1722c13784ce0fe7dc7156631350b6
benedekb97 <benedekb97@gmail.com>
2017-05-15 12:55:54 +0200

    implemented log class in more places

--- new_pipe.php
+++ new_pipe.php
@@ -3,11 +3,18 @@ session_start();
 require_once("includes/init.php");
 require_once("includes/Users.php");
 require_once("includes/Pipes.php");
+require_once("includes/Log.php");
 
 $users = new Users();
 $pipes = new Pipes();
 
 require_once("includes/current_user.php");
+
+$current_pipe = $pipes->getCurrentPipe();
+
+if($current_pipe!=null){
+    header("Location: /");
+}
 
 if (isset($current_user) && ($current_user->getAdmin() || $current_user->getSuperAdmin())) {
     if(isset($_POST['type']) && isset($_POST['time'])){
