47a31e20b7e9f7b2c5e9434b4c4f5353df88587f
benedekb97 <benedekb97@gmail.com>
2017-05-23 17:10:04 +0200

    more logs

--- logout.php
+++ logout.php
@@ -1,5 +1,20 @@
 <?php
 session_start();
+require_once("includes/init.php");
+require_once("includes/Users.php");
+require_once("includes/Log.php");
+
+$users = new Users();
+
+require_once("includes/current_user.php");
+
+if(isset($current_user)){
+    $log = new Log($current_user->getId(),"logout","logout");
+}else{
+    $log = new Log(0,"logout","unauthenticated");
+}
+
+
 unset($_SESSION['uuid']);
 header("Location: /");
 die();
