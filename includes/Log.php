8be20c22661b41232ab3fd018da0bb105fd790f7
benedekb97 <benedekb97@gmail.com>
2017-05-23 17:52:16 +0200

    changed logging script

--- includes/Log.php
+++ includes/Log.php
@@ -7,7 +7,7 @@ class Log
         global $mysql;
         $timestamp = time()-10;
         $ip = $_SERVER['REMOTE_ADDR'];
-        $query = $mysql->query("SELECT * FROM logs WHERE user_id='$user_id' AND ip='$ip' AND timestamp>'$timestamp'");
+        $query = $mysql->query("SELECT * FROM logs WHERE data_changed='$data_changed' AND user_id='$user_id' AND ip='$ip' AND timestamp>'$timestamp'");
         if($query->num_rows == 0){
             $timestamp = time();
             $data_changed = $mysql->real_escape_string($data_changed);
